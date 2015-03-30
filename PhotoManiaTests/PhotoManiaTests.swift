89420c48d341656199b67c36c88a8f4e15b536ba
zhuochuncai <1160090198@qq.com>
2015-03-30 18:35:40 +0800

    Initial Commit

---
+++ PhotoManiaTests/PhotoManiaTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoManiaTests.swift
+//  PhotoManiaTests
+//
+//  Created by zhuochuncai on 15/3/30.
+//  Copyright (c) 2015年 zhuochuncai. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PhotoManiaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
